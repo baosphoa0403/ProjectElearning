6b03fc1678ff01dba12afcf5523ea2d27a7d0007
thaiminhhuy <18520854@gmail.uit.edu.vn>
2020-08-16 00:04:46 -0700

    signup-huy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Navbar from "../src/components/Navbar/index"
-import Carousel from './components/carousel';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// import Navbar from "../src/components/Navbar/index"
+// import Carousel from './components/carousel';
+import SignUp from "./components/SignUp/SignUp";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Carousel />
+      <SignUp />
     </div>
   );
 }
