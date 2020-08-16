677bbbe899700b0374e78f133399a8424980a700
thaiminhhuy <18520854@gmail.uit.edu.vn>
2020-08-16 04:39:36 -0700

    signup-update-function-huy

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-
+import React from "react";
 import Container from "@material-ui/core/Container";
 import {
   BodySignUp,
@@ -12,60 +11,58 @@ import {
   ElementpLoginHere,
   ElementaLoginHere,
 } from "./styled-component-signup";
-export class SignUp extends Component {
-  render() {
-    return (
-      <BodySignUp>
-        <DivBackground>
-          <Container maxWidth="sm">
-            <DivSignUpContent>
-              <form id="signup-form" className="signup-form">
-                <H2SignUp>Create account</H2SignUp>
+function SignUp() {
+  return (
+    <BodySignUp>
+      <DivBackground>
+        <Container maxWidth="sm">
+          <DivSignUpContent>
+            <form id="signup-form" className="signup-form">
+              <H2SignUp>Create account</H2SignUp>
 
-                <DivFormGroup>
-                  <InputFormSignUp type="text" placeholder="Your Email" />
-                </DivFormGroup>
+              <DivFormGroup>
+                <InputFormSignUp type="text" placeholder="Your Email" />
+              </DivFormGroup>
 
-                <DivFormGroup>
-                  <InputFormSignUp type="text" placeholder="Account" />
-                </DivFormGroup>
-                <DivFormGroup>
-                  <InputFormSignUp type="password" placeholder="Password" />
-                </DivFormGroup>
+              <DivFormGroup>
+                <InputFormSignUp type="text" placeholder="Account" />
+              </DivFormGroup>
+              <DivFormGroup>
+                <InputFormSignUp type="password" placeholder="Password" />
+              </DivFormGroup>
 
-                <DivFormGroup>
-                  <InputFormSignUp type="text" placeholder="Your Name" />
-                </DivFormGroup>
-                <DivFormGroup>
-                  <InputFormSignUp type="text" placeholder="Phone Number" />
-                </DivFormGroup>
+              <DivFormGroup>
+                <InputFormSignUp type="text" placeholder="Your Name" />
+              </DivFormGroup>
+              <DivFormGroup>
+                <InputFormSignUp type="text" placeholder="Phone Number" />
+              </DivFormGroup>
 
-                <DivFormGroup>
-                  <InputFormSignUp type="text" placeholder="Code Group" />
-                </DivFormGroup>
-                <DivFormGroup>
-                  <InputSubmitSignUp
-                    type="submit"
-                    placeholder="Code Group"
-                    value="Sign up"
-                  />
-                </DivFormGroup>
-              </form>
-              <ElementpLoginHere>
-                Have already an account ?
-                <ElementaLoginHere
-                  href="https://colorlib.com/etc/regform/colorlib-regform-8/?fbclid=IwAR3sh3S6Rfd_czJxJLv9uGOox4tGVIPs8_T9yyF85Hwrl__IC0sW4QxEH24#"
-                  className="loginhere-link"
-                >
-                  Login here
-                </ElementaLoginHere>
-              </ElementpLoginHere>
-            </DivSignUpContent>
-          </Container>
-        </DivBackground>
-      </BodySignUp>
-    );
-  }
+              <DivFormGroup>
+                <InputFormSignUp type="text" placeholder="Code Group" />
+              </DivFormGroup>
+              <DivFormGroup>
+                <InputSubmitSignUp
+                  type="submit"
+                  placeholder="Code Group"
+                  value="Sign up"
+                />
+              </DivFormGroup>
+            </form>
+            <ElementpLoginHere>
+              Have already an account ?
+              <ElementaLoginHere
+                href="https://colorlib.com/etc/regform/colorlib-regform-8/?fbclid=IwAR3sh3S6Rfd_czJxJLv9uGOox4tGVIPs8_T9yyF85Hwrl__IC0sW4QxEH24#"
+                className="loginhere-link"
+              >
+                Login here
+              </ElementaLoginHere>
+            </ElementpLoginHere>
+          </DivSignUpContent>
+        </Container>
+      </DivBackground>
+    </BodySignUp>
+  );
 }
 
 export default SignUp;
