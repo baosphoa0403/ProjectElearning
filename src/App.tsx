62f1a492f7e14e0d3e75bd5e534829f146197d77
Demo <currykobejames@gmail.com>
2020-08-20 13:28:24 +0700

    fix-bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/Navbar/navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Carousel from "./components/carousel/carousel";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import CourseInfor from "./components/courseInfor/courseInfor"
+
 function App() {
   const [darkMode, setDarkMode] = React.useState(false);
   const darkTheme = createMuiTheme({
@@ -27,10 +27,7 @@ function App() {
         <CssBaseline />
         <Navbar setBackground={setBackground} />
         <Carousel />
-        <CourseInfor/>
       </ThemeProvider>
-      
-     
     </div>
   );
 }
