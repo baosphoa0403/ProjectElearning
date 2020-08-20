98f00889ef2ef8d40def2e11007d5a1fa32c8a90
Demo <currykobejames@gmail.com>
2020-08-20 18:10:16 +0700

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,39 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 import Carousel from "./components/carousel/carousel";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-
+import IntroduceStep from "./components/IntroduceStep_/IntroduceStep";
+import 'font-awesome/css/font-awesome.min.css';
+import CourseInfor from "./components/courseInfor/courseInfor";
 function App() {
   const [darkMode, setDarkMode] = React.useState(false);
-  const darkTheme = createMuiTheme({
+ const darkTheme = createMuiTheme({
     palette: {
-      type: "dark",
-    },
+      type: "dark"
+    }
   });
-  const lightTheme = createMuiTheme({
+ const lightTheme = createMuiTheme({
     palette: {
-      type: "light",
-    },
-  });
+      type: "light"
+    }
+});
   const setBackground = (value: boolean) => {
-    setDarkMode(value);
-  };
-
+     setDarkMode(value)
+  }
+  
   return (
     <div className="App">
       <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        {/* <CssBaseline /> */}
+        <CssBaseline />
+        <Navbar setBackground={setBackground} />
         <Carousel />
+        <CourseInfor/>
+        <IntroduceStep />
       </ThemeProvider>
+
+      {/* <Carousel /> */}
     </div>
   );
 }
