32aaec279d52c11ad8d48a63d10d9a0dc5e11fcb
Demo <currykobejames@gmail.com>
2020-08-20 15:38:19 +0700

    debug3

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
       <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <CssBaseline />
+        {/* <CssBaseline /> */}
         <Carousel />
       </ThemeProvider>
     </div>
