7af4c42e8e032b2b8fdbac9c6dc9a58bb70ebccb
thaiminhhuy <18520854@gmail.uit.edu.vn>
2020-09-09 08:15:13 -0700

    done pagehome

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-import HomePage from "./page/homepage";
+import HomePage from "./page/homepage/home";
 function App() {
   return (
     <div className="App">
--- src/page/homepage/home.tsx
+++ src/page/homepage/home.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import Navbar from "../components/Navbar/Navbar";
-import Carousel from "../components/carousel/carousel";
-import CourseInfor from "../components/courseInfor/courseInfor";
-import IntroduceStep from "../components/IntroduceStep/IntroduceStep";
-import ListCourses from "../components/ListCourses/listCourses";
-import IntroduceTarget from "../components/IntroduceTarget/IntroduceTarget";
-import IntroduceCourse from "../components/IntroduceCourse/IntroduceCourse";
-import Instructors from "../components/Instructors/Instructors";
-import Achievements from "../components/Achievements/Achievements";
+import Navbar from "../../components/Navbar/Navbar";
+import Carousel from "../../components/carousel/carousel";
+import CourseInfor from "../../components/courseInfor/courseInfor";
+import IntroduceStep from "../../components/IntroduceStep/IntroduceStep";
+import ListCourses from "../../components/ListCourses/listCourses";
+import IntroduceTarget from "../../components/IntroduceTarget/IntroduceTarget";
+import IntroduceCourse from "../../components/IntroduceCourse/IntroduceCourse";
+import Instructors from "../../components/Instructors/Instructors";
+import Achievements from "../../components/Achievements/Achievements";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import Footer from "../components/Footer/footer";
+import Footer from "../../components/Footer/footer";
 import CssBaseline from "@material-ui/core/CssBaseline";
 function HomePage() {
   const [darkMode, setDarkMode] = React.useState(false);
