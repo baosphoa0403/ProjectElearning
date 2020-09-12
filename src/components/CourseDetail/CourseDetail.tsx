300df1ccca688b3ea6027bef823c558c08af95cd
thaiminhhuy <18520854@gmail.uit.edu.vn>
2020-09-12 10:50:56 -0700

    doing-course

--- src/components/CourseDetail/CourseDetail.tsx
+++ src/components/CourseDetail/CourseDetail.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { DivProductView, ButtonCard, ButtonBack, Img } from "./StyleDetail";
 import Grid from "@material-ui/core/Grid";
-import imgTest from "../../images/imgTest.png";
+
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
@@ -9,6 +9,11 @@ import Container from "@material-ui/core/Container";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import Cart from "../SearchForListCourses/cart";
 import { RouteComponentProps, withRouter } from "react-router-dom";
+import { rootState } from "../../redux/reducers/Reducers";
+import { connect } from "react-redux";
+import { useEffect } from "react";
+import Axios from "axios";
+
 const useStyles = makeStyles((theme) => ({
   h2: {
     fontSize: "21px",
@@ -54,10 +59,52 @@ interface PropsParams extends RouteComponentProps<{ id: string }> {}
 function CourseDetailChild(props: PropsParams) {
   const classes = useStyles();
   const [isOpen, setOpen] = React.useState(false);
-  console.log(props.match.params);
+  const [newCourse, setNewCourse] = React.useState({
+    maKhoaHoc: "string",
+    biDanh: "string",
+    tenKhoaHoc: "string",
+    moTa: "string",
+    luotXem: Number,
+    hinhAnh: "string",
+    maNhom: "string",
+    ngayTao: "string",
+    soLuongHocVien: Number,
+    nguoiTao: {
+      taiKhoan: "string",
+      hoTen: "string",
+      maLoaiNguoiDung: "string",
+      tenLoaiNguoiDung: "string",
+    },
+    danhMucKhoaHoc: {
+      maDanhMucKhoahoc: "string",
+      tenDanhMucKhoaHoc: "string",
+    },
+  });
+  let id = props.match.params.id;
+  console.log(newCourse);
 
+  useEffect(() => {
+    console.log(id);
+
+    Axios({
+      method: "GET",
+      url: `http://elearning0706.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayThongTinKhoaHoc?maKhoaHoc=${id}`,
+    })
+      .then((res: any) => {
+        setNewCourse(res.data);
+      })
+      .catch((err: any) => {
+        console.log(err.request);
+      });
+  }, []);
   return (
     <React.Fragment>
+      <ButtonBack>
+        <ArrowBackIcon className={classes.icon} />
+        <Typography component="span" variant="button">
+          Back
+        </Typography>
+      </ButtonBack>
       <Container>
         <DivProductView>
           <Grid
@@ -67,22 +114,25 @@ function CourseDetailChild(props: PropsParams) {
             alignItems="flex-start"
           >
             <Grid lg={6} xl={6} md={6}>
-              <ButtonBack>
-                <ArrowBackIcon className={classes.icon} />
-                <Typography component="span" variant="button">
-                  Back
-                </Typography>
-              </ButtonBack>
               <Img>
-                <img src={imgTest} width="100%" height="100%" alt="" />
+                <img
+                  src={newCourse.hinhAnh}
+                  width="100%"
+                  height="100%"
+                  alt=""
+                />
               </Img>
             </Grid>
             <Grid lg={6} xl={6} md={6}>
               <Typography variant="h5" component="h2" className={classes.h2}>
-                The Golden Treasury of Children Literature
+                {newCourse.biDanh}
               </Typography>
-              <Typography variant="caption" component="p" className={classes.p}>
-                By Skipton Stitwell
+              <Typography
+                variant="caption"
+                component="span"
+                className={classes.p}
+              >
+                By {newCourse.nguoiTao.hoTen}
               </Typography>
               <Typography
                 variant="body1"
@@ -101,13 +151,6 @@ function CourseDetailChild(props: PropsParams) {
                 elementum nullam varius nulla facilisi cras non velit nec nisi
                 vulputate nonummy maecenas tincidunt lacus at velit vivamus vel
                 nulla eget eros elementum pellentesque quisque porta volutpat
-                erat quisque erat eros viverra eget congue eget semper rutrum
-                nulla nunc purus phasellus in felis donec semper sapien a libero
-                nam dui proin leo odio porttitor id consequat in consequat ut
-                nulla sed accumsan felis ut at dolor quis odio consequat varius
-                integer ac leo pellentesque ultrices mattis odio donec vitae
-                nisi nam ultrices libero non mattis pulvinar nulla pede
-                ullamcorper augue a suscipit
               </Typography>
               <Typography
                 component="div"
@@ -137,9 +180,18 @@ function CourseDetailChild(props: PropsParams) {
           </Grid>
         </DivProductView>
       </Container>
+
       <Cart />
     </React.Fragment>
   );
 }
+// const mapStateToProps = (state: rootState) => {
+//   return {
+//     newDetailCourse: state.detailCourseReducer.detailCourse,
+//   };
+// };
+// const mapDispatchToProps = (dispatch: any) => {
+//   return {};
+// };
 const CourseDetail = withRouter(CourseDetailChild as any); //Note: It is a workaround not an actual solution
 export default CourseDetail;
--- src/redux/reducers/Reducers.tsx
+++ src/redux/reducers/Reducers.tsx
@@ -1,13 +1,15 @@
 import { combineReducers } from "redux";
 import testReducer from "../../components/Test/Modules/Reducers/Reducers";
 import SignInReducer from "../../components/FormSignIn/Modules/Reducers/Reducers";
-import SignUpsReducer from "../../components/SignUp/modules/Reducers/Reducers"
+import SignUpsReducer from "../../components/SignUp/modules/Reducers/Reducers";
 import reducerHome from "../../page/homepage/module/reducers/reducer";
+import detailCourseReducer from "../../page/detailcourses/module/reducer/Reducer";
 const rootReducer = combineReducers({
   testReducer,
   reducerHome,
   SignInReducer,
-  SignUpsReducer
+  SignUpsReducer,
+  detailCourseReducer,
 });
 
 export type rootState = ReturnType<typeof rootReducer>;
