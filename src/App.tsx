b317c19abd560660a5dd7d6a279484271bcd0ddc
bảo <baosphoa0403@gmail.com>
2020-09-22 21:48:16 +0700

    design routeAdmin

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
-
+import {routesAdmin} from "./routes/admin"
 import { routesHome } from "./routes/index";
 import { HomeTemplate } from "./templates/HomeTemplate";
+import {AdminTemplate} from "./templates/AdminTemplate"
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Auth from "./page/admin/auth/auth";
 function App() {
   const showHomeLayout = (routesHome: any) => {
     if (routesHome && routesHome.length > 0) {
@@ -19,13 +21,27 @@ function App() {
       });
     }
   };
+  const showAdminLayout = (routesAdmin: any) => {
+    if (routesAdmin && routesAdmin.length > 0) {
+      return routesAdmin.map((item: any, index: any) => {
+        return (
+          <AdminTemplate
+            key={index}
+            exact={item.exact}
+            path={item.path}
+            Component={item.component}
+          />
+        );
+      });
+    }
+  };
   return (
     <BrowserRouter>
       <Switch>
         <Route>
           {showHomeLayout(routesHome)}
-
-          {/* <Route path="/admin" component={Admin} exact={false} /> */}
+           {showAdminLayout(routesAdmin)}
+          <Route path="/admin" component={Auth} exact={false} />
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ src/page/admin/admin.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AdminPage() {
+    return (
+        <React.Fragment>
+            <h1>test auth</h1>
+        </React.Fragment>
+    )
+}
